72a1879ba044f5ca748bf2e5ea567443189f15c1
joseperu2503 <joseperu2503@gmail.com>
2024-05-31 01:47:35 -0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:moviesplus/config/constants/app_colors.dart';
 import 'package:moviesplus/config/constants/environment.dart';
 import 'package:moviesplus/config/router/app_router.dart';
 import 'package:moviesplus/config/theme/app_theme.dart';
 
 void main() async {
   await Environment.initEnvironment();
+
+  SystemChrome.setSystemUIOverlayStyle(
+    const SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+      systemNavigationBarColor: AppColors.backgroundColor,
+    ),
+  );
 
   runApp(
     const ProviderScope(
