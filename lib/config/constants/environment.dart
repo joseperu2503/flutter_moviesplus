b7152b0d3991a36122d1dc6fa431b4af4df5c028
joseperu2503 <joseperu2503@gmail.com>
2024-06-04 18:01:49 -0500

    web env

--- lib/config/constants/environment.dart
+++ lib/config/constants/environment.dart
@@ -2,7 +2,7 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class Environment {
   static initEnvironment() async {
-    await dotenv.load();
+    await dotenv.load(fileName: '.dotenv');
   }
 
   static String movieApiKey = dotenv.get('MOVIE_API_KEY');
