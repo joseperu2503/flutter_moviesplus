249f457321d2265bc788367ae2d9897dc98ff5be
joseperu2503 <joseperu2503@gmail.com>
2024-12-18 13:21:30 -0500

    Update movie_buttons.dart

--- lib/features/movie/widgets/movie_buttons.dart
+++ lib/features/movie/widgets/movie_buttons.dart
@@ -26,67 +26,67 @@ class _MovieButtonsState extends State<MovieButtons> {
   Widget build(BuildContext context) {
     return Row(
       children: [
-        TextButton(
-          onPressed: () {},
-          style: TextButton.styleFrom(
-            backgroundColor: AppColors.secondaryOrange,
-            minimumSize: const Size(115, 48),
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(32),
-            ),
-          ),
-          child: Row(
-            children: [
-              SvgPicture.asset(
-                'assets/icons/play.svg',
-                width: 18,
-                height: 18,
-                colorFilter: const ColorFilter.mode(
-                  AppColors.white,
-                  BlendMode.srcIn,
-                ),
-              ),
-              const SizedBox(
-                width: 8,
-              ),
-              const Text(
-                'Play',
-                style: TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.w500,
-                  color: AppColors.white,
-                  height: 19.5 / 16,
-                  letterSpacing: 0.12,
-                  leadingDistribution: TextLeadingDistribution.even,
-                ),
-              ),
-            ],
-          ),
-        ),
+        // TextButton(
+        //   onPressed: () {},
+        //   style: TextButton.styleFrom(
+        //     backgroundColor: AppColors.secondaryOrange,
+        //     minimumSize: const Size(115, 48),
+        //     shape: RoundedRectangleBorder(
+        //       borderRadius: BorderRadius.circular(32),
+        //     ),
+        //   ),
+        //   child: Row(
+        //     children: [
+        //       SvgPicture.asset(
+        //         'assets/icons/play.svg',
+        //         width: 18,
+        //         height: 18,
+        //         colorFilter: const ColorFilter.mode(
+        //           AppColors.white,
+        //           BlendMode.srcIn,
+        //         ),
+        //       ),
+        //       const SizedBox(
+        //         width: 8,
+        //       ),
+        //       const Text(
+        //         'Play',
+        //         style: TextStyle(
+        //           fontSize: 16,
+        //           fontWeight: FontWeight.w500,
+        //           color: AppColors.white,
+        //           height: 19.5 / 16,
+        //           letterSpacing: 0.12,
+        //           leadingDistribution: TextLeadingDistribution.even,
+        //         ),
+        //       ),
+        //     ],
+        //   ),
+        // ),
         const SizedBox(
           width: 16,
         ),
         const Spacer(),
-        TextButton(
-          onPressed: () {},
-          style: TextButton.styleFrom(
-            backgroundColor: AppColors.primarySoft,
-            minimumSize: const Size(48, 48),
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(32),
-            ),
-            padding: EdgeInsets.zero,
-          ),
-          child: SvgPicture.asset(
-            'assets/icons/download.svg',
-            width: 24,
-            height: 24,
-            colorFilter: const ColorFilter.mode(
-              AppColors.secondaryOrange,
-              BlendMode.srcIn,
-            ),
-          ),
-        ),
+        // TextButton(
+        //   onPressed: () {},
+        //   style: TextButton.styleFrom(
+        //     backgroundColor: AppColors.primarySoft,
+        //     minimumSize: const Size(48, 48),
+        //     shape: RoundedRectangleBorder(
+        //       borderRadius: BorderRadius.circular(32),
+        //     ),
+        //     padding: EdgeInsets.zero,
+        //   ),
+        //   child: SvgPicture.asset(
+        //     'assets/icons/download.svg',
+        //     width: 24,
+        //     height: 24,
+        //     colorFilter: const ColorFilter.mode(
+        //       AppColors.secondaryOrange,
+        //       BlendMode.srcIn,
+        //     ),
+        //   ),
+        // ),
         const SizedBox(
           width: 16,
         ),
